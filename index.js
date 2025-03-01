dd98629eac57a4462362e9ae3d32c2d285300f5a
devinscott16 <devin.michael.scott@gmail.com>
2025-03-01 09:39:59 -0700

    basic outline w/o alrt

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-console.log ("testing")
+// Creat a game of Tic Tac Toe
+// X or O should appear in the spot clicked (being turn based)
+// A heading to say whos turn it is on each move
+// A 'Clear' Button
+// Bootstrap compoent(s) to define who won / if it is a draw
